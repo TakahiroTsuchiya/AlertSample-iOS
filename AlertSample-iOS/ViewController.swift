6535a9c6b2de649e923695f671217dffc11086ed
Takahiro Tsuchiya <takahirotsuchiya1984@icloud.com>
2017-10-20 13:58:59 +0900

    add Alert Dialog.

--- AlertSample-iOS/ViewController.swift
+++ AlertSample-iOS/ViewController.swift
@@ -20,6 +20,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: Action
+    @IBAction func showAction(_ sender: Any) {
+        let alert = UIAlertController(title:"タイトル", message: "メッセージ", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let action1 = UIAlertAction(title: "アクション１", style: UIAlertActionStyle.default, handler: {
+            (action: UIAlertAction!) in
+            print("アクション１をタップした時の処理")
+        })
+        
+        let action2 = UIAlertAction(title: "アクション２", style: UIAlertActionStyle.default, handler: {
+            (action: UIAlertAction!) in
+            print("アクション２をタップした時の処理")
+        })
+        
+        let action3 = UIAlertAction(title: "アクション３", style: UIAlertActionStyle.destructive, handler: {
+            (action: UIAlertAction!) in
+            print("アクション３をタップした時の処理")
+        })
+        
+        let cancel = UIAlertAction(title: "キャンセル", style: UIAlertActionStyle.cancel, handler: {
+            (action: UIAlertAction!) in
+            print("キャンセルをタップした時の処理")
+        })
+        
+        alert.addAction(action1)
+        alert.addAction(action2)
+        alert.addAction(action3)
+        alert.addAction(cancel)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
